817bdef813adfc64cf75bcde9f416eeb558c5830
Gilangp <gilangpurnomo0505@gmail.com>
2026-08-07 10:32:10 +0700

    fix lagi

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+
+export default defineConfig([
+  ...nextVitals,
+  {
+    ignores: [
+      ".next/**",
+      "out/**",
+      "build/**",
+      "dist/**",
+      "coverage/**",
+      "next-env.d.ts",
+    ],
+  },
+]);
